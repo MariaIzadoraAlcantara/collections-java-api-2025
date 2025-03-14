d4021cc91f160c0d874601562cd105244f35ed73
mariaizadora <mariaizadora_1996@hotmail.com>
2025-03-14 18:27:55 +0000

    Exercício Tarefa

---
+++ src/main/java/list/OperacoesBasicas/ListaTarefa.java
@@ -0,0 +1,53 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasParaRemover = new ArrayList<>();
+        for(Tarefa t : tarefaList){
+            if(t.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasParaRemover.add(t);
+            }
+        }
+        tarefaList.removeAll(tarefasParaRemover);
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
+
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        listaTarefa.adicionarTarefa("Acordar");
+        listaTarefa.adicionarTarefa("Tomar Café");
+        listaTarefa.adicionarTarefa("Codar");
+        listaTarefa.adicionarTarefa("Almoçar");
+        listaTarefa.adicionarTarefa("Codar");
+
+        System.out.println("O número de elementos na minha lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Codar");
+        System.out.println("O número de elementos na minha lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+
+
+    }
+}
